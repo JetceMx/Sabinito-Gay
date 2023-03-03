b94ba2d556df0d23f079f7bb1d899ea439a59e54
Juan Manuel González Alaniz <j.mannuelg.a@gmail.com>
2023-03-02 23:53:37 -0600

    Intentando crear el inicio

---
+++ js/inicio.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    
+
+    var c = document.getElementById("text");
+    var ctx = c.getContext("2d");
+    ctx.font = "30px Arial";
+    ctx.fillText("Hello World",10,50);
+}
