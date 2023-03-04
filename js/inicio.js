4db2c2a2f650d489783d8630f945db0b041bf2d9
Juan Manuel González Alaniz <j.mannuelg.a@gmail.com>
2023-03-03 21:17:49 -0600

    Xd

--- js/inicio.js
+++ js/inicio.js
@@ -5,12 +5,12 @@ var img2 = new Image();
 img2.src = "images/Logo.png";
 
 var img3 = new Image();
-img3.src = "images/Start.png";
+window.open(img3.src = "images/Start.png");
 
 window.onload = function(){
     var canvas = document.getElementById("lienzo");
     var ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0);
-    ctx.drawImage(img2, 460,70,400,400);
-    ctx.drawImage(img3,600,450,150,150);
+    ctx.drawImage(img2, 470,70,400,400);
+    ctx.drawImage(img3,610,450,150,150);
 }
