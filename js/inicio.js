f7042f2619213012250e193c14e100ba9a3c4354
Juan Manuel González Alaniz <j.mannuelg.a@gmail.com>
2023-03-03 20:01:53 -0600

    Diseño de la pagina de inicio

--- js/inicio.js
+++ js/inicio.js
@@ -1,8 +1,16 @@
-window.onload = function(){
-    
+var img = new Image();
+img.src = "images/Granja.jpg";
 
-    var c = document.getElementById("text");
-    var ctx = c.getContext("2d");
-    ctx.font = "30px Arial";
-    ctx.fillText("Hello World",10,50);
+var img2 = new Image();
+img2.src = "images/Logo.png";
+
+var img3 = new Image();
+img3.src = "images/Start.png";
+
+window.onload = function(){
+    var canvas = document.getElementById("lienzo");
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img2, 460,70,400,400);
+    ctx.drawImage(img3,600,450,150,150);
 }
